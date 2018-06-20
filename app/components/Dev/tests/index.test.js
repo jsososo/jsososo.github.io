624d1ed6ed3a0bd2344f9441b62998c15f4039e9
jinshenou <jinshenou@raycloud.com>
2018-06-20 15:39:58 +0800

    update: 后台

---
+++ app/components/Dev/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import Dev from '../index';
+
+describe('<Dev />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
