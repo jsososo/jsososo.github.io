6a8d79aa17131af72aff7b19acad8b96941587bb
jinshenou <jinshenou@raycloud.com>
2018-07-15 18:04:27 +0800

    update: del pwa

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -12,8 +12,8 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
-    filename: 'person-web/[name].js',
-    chunkFilename: 'person-web/[name].chunk.js',
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
   },
 
   plugins: [
