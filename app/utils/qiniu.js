5f0c08ded6618a071cae1d9e261399ddfc9a0a9d
jsososo <jsososo@outlook.com>
2019-05-07 23:13:28 +0800

    update: 文件名

--- app/utils/qiniu.js
+++ app/utils/qiniu.js
@@ -1,5 +1,7 @@
 import * as qiniu from 'qiniu-js';
 import { QN_TOKEN as token } from "../const";
+import md5 from 'js-md5';
+import timer from './timer';
 
 const putExtra = {
   fname: '',
@@ -7,10 +9,17 @@ const putExtra = {
   mimeType: null,
 };
 
-var config = {
+const config = {
   useCdnDomain: false,
   disableStatisticsReport: false,
   retryCount: 3,
 };
 
-export default (file) => qiniu.upload(file, file.name, token, putExtra, config);
+const getFileName = (name = '') => {
+  const a = name.split('.');
+  const after = a[a.length - 1];
+  const now = timer();
+  return `${now.str('YYMMDD/HHmmss/')}${md5(now.time + (Math.random() * 1000) + '')}.${after}`;
+};
+
+export default (file, filename) => qiniu.upload(file, filename || getFileName(file.name), token, putExtra, config);
